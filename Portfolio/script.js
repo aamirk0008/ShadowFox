8150fc70ee1ac3d0e15768d878134f183e55d9e7
aamirk0008 <113338901+aamirk0008@users.noreply.github.com>
2024-04-10 04:35:22 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+})
